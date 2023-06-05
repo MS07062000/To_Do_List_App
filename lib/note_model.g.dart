eddacf072b009f18b6958a90e59370f2212d7d39
MS07062000 <singhmurali.14@gmail.com>
2023-06-05 23:57:22 +0530

    Generated note_model.g.dart

---
+++ lib/note_model.g.dart
@@ -0,0 +1,50 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'note_model.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class NoteModelAdapter extends TypeAdapter<NoteModel> {
+  @override
+  final int typeId = 0;
+
+  @override
+  NoteModel read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return NoteModel(
+      destination: fields[0] as String,
+      notetitle: fields[1] as String,
+      textnote: fields[2] as String?,
+      checklist: (fields[3] as List?)?.cast<String>(),
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, NoteModel obj) {
+    writer
+      ..writeByte(4)
+      ..writeByte(0)
+      ..write(obj.destination)
+      ..writeByte(1)
+      ..write(obj.notetitle)
+      ..writeByte(2)
+      ..write(obj.textnote)
+      ..writeByte(3)
+      ..write(obj.checklist);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is NoteModelAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
